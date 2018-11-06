be4272234cd0f372017c514f28d393ce5eec1dc3
Scott Walter <swalter@cray.com>
2018-11-06 20:45:58 +0000

    initial commit

---
+++ miniac_bot.py
@@ -0,0 +1,331 @@
+#!/usr/bin/python3
+
+import discord
+import sqlite3
+import re
+import asyncio
+from sqlite3 import Error
+
+TOKEN = ''
+client = discord.Client()
+database = "./points.db"
+
+def create_user_table(user, conn):
+    """
+    Create a table for a discord user that will contain links to their images.
+    :param user: string discord user
+    :param conn: connection to database
+    :return: nothing
+    """
+    create_user_query = """CREATE TABLE IF NOT EXISTS '{0}' (
+                             id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                             link text
+                           )""".format(user)
+    if conn is not None:
+        try:
+            with conn:
+                cur = conn.cursor()
+                cur.execute(create_user_query)
+        except Error as e:
+            print("Failed to create user {0}. There error is below.".format(user))
+            print(e)
+    else:
+        print("Error! Database connection was not established before creating a user's table.")
+
+def create_leaderboard_table(conn):
+    """
+    This method creates the leaderboard table. It shouldn't need to be run more than once per year when the table resets.
+    :param conn: connection to database
+    :return: nothing
+    """
+    create_leaderboard_query = """CREATE TABLE IF NOT EXISTS leaderboard (
+                                    id integer PRIMARY KEY AUTOINCREMENT,
+                                    user text NOT NULL,
+                                    points integer NOT NULL
+                                  )"""
+    if conn is not None:
+        try:
+            with conn:
+                cur = conn.cursor()
+                cur.execute(create_leaderboard_query)
+        except Error as e:
+            print("Failed to create leaderboard table. The error is below.")
+            print(e)
+    else:
+        print("Error! Database connection was not established before creating leaderboard table.")
+
+def insert_link(user, link, conn):
+    """
+    Insert a link into a discord user's table
+    :param user: string discord user
+    :param link: a link to the image
+    :param conn: connection to the database
+    :return: nothing
+    """
+    create_user_table(user, conn)
+    insert_query = "INSERT INTO '{}' (link) VALUES ('{}')".format(user, link)
+    if conn is not None:
+        try:
+            with conn:
+                cur = conn.cursor()
+                cur.execute(insert_query)
+        except Error as e:
+            print("Failed to add image to {0}'s gallery. There error is below.".format(user))
+            print(e)
+    else:
+        print("Error! Database connection was not established before adding to a user's gallery.")
+
+def find_user(user, conn):
+    """
+    Find a user in the leaderboard
+    :param user: string discord user
+    :param conn: connection to the database
+    :return: a boolean as to whether or not the user exists in the database. Can be null if query fails.
+    """
+    find_query = "SELECT rowid FROM leaderboard WHERE user = '{}'".format(user)
+    if conn is not None:
+        try:
+            cur = conn.cursor()
+            cur.execute(find_query)
+            data = cur.fetchall()
+            return bool(len(data))
+        except Error as e:
+            print("Failed when querying to find {}. The error is below.".format(user))
+            print(e)
+    else:
+        print("Error! Database connection was not established when trying to find a user")
+
+def increment_points(user, points, conn):
+    """
+    Increment a user's points by a specific value
+    :param user: an ID for a discord user
+    :param points: int value
+    :param conn: connection to the database
+    :return: user's point total
+    """
+    increment_query = "UPDATE leaderboard SET points = points + {0} WHERE user = '{1}'".format(points, user)
+    create_user_query = "INSERT INTO leaderboard (points, user) VALUES ({0}, '{1}')".format(points, user)
+    get_user_point = "SELECT points FROM leaderboard WHERE user='{}'".format(user)
+    if conn is not None:
+        if find_user(user, conn):
+            try:
+                with conn:
+                    cur = conn.cursor()
+                    cur.execute(increment_query)
+            except Error as e:
+                print('Failed to increment {}\'s points. Error is below.'.format(user))
+                print(e)
+        else:
+            try:
+                with conn:
+                    cur = conn.cursor()
+                    cur.execute(create_user_query)
+            except Error as e:
+                print('Failed to create {} in leaderboard. Error is below.'.format(user))
+                print(e)
+        try:
+            with conn:
+                cur = conn.cursor()
+                cur.execute(get_user_point)
+                return cur.fetchone()[0]
+
+        except Error as e:
+            print('Failed to get {}\'s current points. Error is below.'.format(user))
+            print(e)
+    else:
+        print("Error! Database connection was not established when incrementing a user's point total")
+
+def retrieve_sorted_leaderboard(conn):
+    """
+    Returns a list of sorted users highest to lowest organized by their point values.
+    :param: conn: connection to the database
+    :return: list
+    """
+    get_sorted_leaderboard_query = "SELECT user, points FROM leaderboard ORDER BY points DESC"
+    if conn is not None:
+        try:
+            cur = conn.cursor()
+            cur.execute(get_sorted_leaderboard_query)
+            return cur.fetchall()
+        except Error as e:
+            print('Failed to retrieve the leaderboard. Error is below.')
+            print(e)
+    else:
+        print("Error! Database connection was not established when querying the order of the leaderboard.")
+
+def retrieve_gallery(user, conn):
+    """
+    This returns the content of a user's gallery table.
+    :param user: the ID of a discord user
+    :param conn: connection to the database
+    :return: returns a list of tuples
+    """
+    get_gallery_query = "SELECT link FROM '{}'".format(user)
+    if conn is not None:
+        try:
+            cur = conn.cursor()
+            cur.execute(get_gallery_query)
+            return cur.fetchall()
+        except Error as e:
+            print('Failed to retrieve {}\'s gallery. Error is below.'.format(user))
+            print(e)
+    else:
+        print("Error! Database connection was not established when querying a user's gallery.")
+
+def increment_points_wrapper(message):
+    """
+    This is a wrapper for the function to add points to a discord user.
+    :param message: this is a discord message containing all the params to run this function
+    :return: a message to print out in discord
+    """
+    
+    # The message to print out in discord 
+    return_message = ''
+    roles = []
+    for role in message.author.roles:
+        roles.append(role.name)
+
+    if 'Wight King' not in roles and 'Thrall' not in roles:
+        # ah ah ah!
+        return_message = 'http://gph.is/15wY87J'
+        return return_message
+
+    # split out the various params
+    command_params = message.content.split()
+    if len(command_params) != 4:
+        return_message = 'You\'re missing a parameter. Please see the !brian documentation'
+        return return_message
+
+    if '@' not in command_params[1]:
+        return_message = 'You need to tag a user with this command. Their name should appear blue in discord.'
+        return return_message
+
+    if not command_params[2].isdigit():
+        return_message = 'You need to use an integer when giving a user points.'
+        return return_message
+
+    command = command_params[0]
+    # remove non digit characters like !, @, <, or >
+    discord_user_id = re.sub("\D", "", command_params[1])
+    points = command_params[2]
+    image_link = command_params[3]
+    
+    conn = sqlite3.connect(database)
+    user_points = increment_points(discord_user_id, points, conn)
+    insert_link(discord_user_id, image_link, conn)
+    conn.close
+    if user_points == 1:
+        return_message = ":metal:Congratulations, {}. You're on the board with 1 point!:metal:".format(message.server.get_member(discord_user_id).display_name)
+    else:
+        return_message = ":metal:Congratulations, {}. You now have {} points:metal:".format(message.server.get_member(discord_user_id).display_name,user_points)
+
+    return return_message
+
+def get_leaderboard(message):
+    conn = sqlite3.connect(database)
+    leaderboard = retrieve_sorted_leaderboard(conn)
+    conn.close()
+    discord_message = ''
+    if leaderboard is None:
+        print("The leaderboard table does not exist.")
+    else:
+        for user in leaderboard:
+            discord_message += '{}: {}\n'.format(message.server.get_member(user[0]).display_name, user[1])
+        if len(leaderboard):
+            return '```{}```'.format(discord_message)
+        else:
+            return '```leaderboard is empty.```'.format(discord_message)
+
+def get_gallery(message):
+    # split out the various params
+    command_params = message.content.split()
+    if len(command_params) != 2:
+        return_message = 'You\'re missing a parameter. Please see the !help documentation'
+        return return_message
+
+    if '@' not in command_params[1]:
+        return_message = 'You need to tag a user with this command. Their name should appear blue in discord.'
+        return return_message
+
+    command = command_params[0]
+    # remove non digit characters like !, @, <, or >
+    discord_user_id = re.sub("\D", "", command_params[1])
+    conn = sqlite3.connect(database)
+    gallery = retrieve_gallery(discord_user_id, conn)
+    conn.close()
+    discord_private_message = ''
+    index = 1
+    for link in gallery:
+        discord_private_message += '{}. {}\n'.format(index, link[0])
+        index += 1
+
+    return discord_private_message
+
+def brian():
+    return_message = """
+    Hi, I'm Brian. You can call me Bryguy. I'm you're friendly bot companion. Here are the commands you can do: \n
+    `!leaderboard`\n
+    This returns the current point totals for everyone in the discord who has at least one point.\n
+    `!gallery [discord_user]`\n
+    This private messages you a discord user's personal gallery. These are all the pictures they've gotten points for. Make sure to actually tag the user, their name should appear blue.\n
+    `!7years` \n
+    Never do this.\n
+    `!add [discord_user] [points] [link]`\n
+    Only Wight Kings and Thralls can run this command. This increments your point total by [points], and adds a new image to your gallery.\n
+    `!brian`
+    This prints this message!
+    """
+    return return_message
+
+async def boot_non_roles():
+        await client.wait_until_ready()
+        miniac_server = ''
+        keeper_roles = {'Wight King','Patreon','Rythm','Executioner','Zombie','Moose Fanclub','Dark Wizard','Acolyte','Zombie','Sepulchral Guard'}
+        for server in client.servers:
+            if server.name == 'Miniac':
+                miniac_server = server
+        
+        boot = list()
+        for member in miniac_server.members:
+            roles = set()
+            for miniac_role in member.roles:
+                roles.add(miniac_role.name)
+            if not (keeper_roles & roles):
+                boot.append(member)
+
+        test_shit_general_channel = discord.Object(id='502579735013097483')
+        while not client.is_closed:
+            for person in boot:
+                await client.kick(person)
+            await asyncio.sleep(86400) # task runs once a day
+
+@client.event
+async def on_message(message):
+    # Find string versions of the name and add them to a list
+
+    if message.content.startswith('!add'):
+        discord_message = increment_points_wrapper(message)
+        await client.send_message(message.channel, discord_message)
+
+    if message.content.startswith('!leaderboard'):
+        discord_message = get_leaderboard(message)
+        await client.send_message(message.channel, discord_message)
+
+    if message.content.startswith('!gallery'):
+        discord_private_message = get_gallery(message)
+        await client.send_message(message.author, discord_private_message)
+
+    if message.content.startswith("!7years"):
+        await client.send_message(message.channel, 'https://i.imgur.com/0u1Uw3f.gif')
+
+    if message.content.startswith("!brian"):
+        await client.send_message(message.channel, '{}'.format(brian()))
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+client.loop.create_task(boot_non_roles())
+client.run(TOKEN)
